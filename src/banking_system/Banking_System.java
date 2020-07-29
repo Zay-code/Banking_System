c81fd873b6e3ab9748337e2dafde4a1ca50f3708
Jenny-Coleman <interncptwex015@capaciti.org.za>
2020-07-29 17:43:11 +0200

    Calculator added

--- src/banking_system/Banking_System.java
+++ src/banking_system/Banking_System.java
@@ -15,7 +15,7 @@ public class Banking_System {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
