fa9a8865ef03bbeadb8a82c20f85eb04e88a3062
Zay-code <62377930+Zay-code@users.noreply.github.com>
2020-07-30 20:41:39 +0200

    Update Banking_System.java

--- src/banking_system/Banking_System.java
+++ src/banking_system/Banking_System.java
@@ -15,7 +15,8 @@ public class Banking_System {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        SignIn frmSignIn = new SignIn();
+        frmSignIn.setVisible(true);
     }
     
 }
